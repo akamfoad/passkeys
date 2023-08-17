b02dff158b92c02295a81da634c2f1e12a01552b
akamfoad <akamfanaye@gmail.com>
2023-08-17 20:48:37 +0300

    add: spinner to login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,11 +1,12 @@
 import classNames from "classnames";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { json, type ActionArgs, redirect } from "@remix-run/node";
 import {
   Form,
   Link,
   useActionData,
-  useNavigate,
+  useFormAction,
+  useNavigation,
   useSearchParams,
 } from "@remix-run/react";
 
@@ -52,10 +53,13 @@ export const action = async ({ request }: ActionArgs) => {
 };
 
 const Login = () => {
-  const navigate = useNavigate();
+  const navigation = useNavigation();
   const congratulateeRef = useRef<HTMLDivElement | null>(null);
   const errors = useActionData<typeof action>();
   const [searchParams, setSearchParams] = useSearchParams();
+  const [authenticatingWithPasskey, setAuthenticatingWithPasskey] =
+    useState(false);
+  const loginFormAction = useFormAction(".", { relative: "path" });
 
   const congratulatee = searchParams.get("congratulations");
 
@@ -92,7 +96,12 @@ const Login = () => {
     });
   }, []);
 
+  const isLoggingIn =
+    navigation.state !== "idle" && navigation.formAction === loginFormAction;
+
   const loginWithPasskeys = async () => {
+    setAuthenticatingWithPasskey(true);
+
     const resp = await fetch("/actions/passkeys/authentication");
     const { options } = await resp.json();
 
@@ -100,6 +109,7 @@ const Login = () => {
     try {
       asseResp = await startAuthentication(options);
     } catch (error) {
+      setAuthenticatingWithPasskey(false);
       throw error;
     }
 
@@ -120,6 +130,8 @@ const Login = () => {
         window.location.pathname = "/";
       }, 250);
     }
+
+    setAuthenticatingWithPasskey(false);
   };
 
   return (
@@ -175,9 +187,32 @@ const Login = () => {
               <button
                 onClick={loginWithPasskeys}
                 type="button"
-                className="aspect-square p-2 rounded-lg text-slate-600 hover:bg-slate-500/10 flex items-center justify-center absolute right-3 inset-y-0 my-auto"
+                disabled={authenticatingWithPasskey}
+                className={classNames(
+                  "flex items-center justify-center absolute right-3 inset-y-0 my-auto",
+                  "aspect-square p-2 rounded-lg text-slate-600 hover:bg-slate-500/10",
+                  {
+                    "bg-slate-500/10": authenticatingWithPasskey,
+                    hidden: isLoggingIn && !authenticatingWithPasskey,
+                  }
+                )}
               >
-                <Icon height={16} />
+                {authenticatingWithPasskey ? (
+                  <svg
+                    aria-hidden="true"
+                    className="inline w-4 h-4 animate-spin text-transparent fill-emerald-900"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                ) : (
+                  <Icon height={16} />
+                )}
               </button>
             </div>
           </div>
